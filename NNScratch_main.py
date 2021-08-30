92cb98fa5c40bf70d48105e71ebe250bd630ef69
Vladm0z <Vladm0z@users.noreply.github.com>
2021-08-30 22:11:00 +0300

    Adam

--- NNScratch_main.py
+++ NNScratch_main.py
@@ -249,6 +249,45 @@ class Optimizer_Adagrad:
 	def post_update_params(self):
 		self.iterations += 1
 
+class Optimizer_Adam:
+	def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
+		self.learning_rate = learning_rate
+		self.current_learning_rate = learning_rate
+		self.decay = decay
+		self.iterations = 0
+		self.epsilon = epsilon
+		self.beta_1 = beta_1
+		self.beta_2 = beta_2
+
+	def pre_update_params(self):
+		if self.decay:
+			self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+	def update_params(self, layer):
+		if not hasattr(layer, 'weight_cache'):
+			layer.weight_momentums = np.zeros_like(layer.weights)
+			layer.weight_cache = np.zeros_like(layer.weights)
+			layer.bias_momentums = np.zeros_like(layer.biases)
+			layer.bias_cache = np.zeros_like(layer.biases)
+
+		layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.dweights
+		layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.dbiases
+
+		weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+		bias_momentums_corrected = layer.bias_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+		
+		layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.dweights**2 
+		layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.dbiases**2 
+
+		weight_cache_corrected = layer.weight_cache / (1 - self.beta_2 ** (self.iterations + 1))
+		bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
+
+		layer.weights += -self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
+		layer.biases += -self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+
+	def post_update_params(self):
+		self.iterations += 1 		
+		
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 
@@ -266,7 +305,7 @@ dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
 #Create optimizer
-optimizer = Optimizer_Adagrad(decay=1e-4)
+optimizer = Optimizer_Adam(learning_rate = 0.02, decay=1e-5)
 
 #Train in loop
 for epoch in range(10001):
@@ -309,3 +348,32 @@ for epoch in range(10001):
 	optimizer.update_params(dense1)
 	optimizer.update_params(dense2)
 	optimizer.post_update_params()
+
+
+# Validate the model
+# Create test dataset
+X_test, y_test = spiral_data(samples=100, classes=3)
+
+# Perform a forward pass of our testing data through this layer
+dense1.forward(X_test)
+
+# Perform a forward pass through activation function
+# takes the output of first dense layer here
+activation1.forward(dense1.output)
+
+# Perform a forward pass through second Dense layer
+# takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+
+# Perform a forward pass through the activation/loss function
+# takes the output of second dense layer here and returns loss
+loss = loss_activation.forward(dense2.output, y_test)
+
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y_test.shape) == 2:
+	y_test = np.argmax(y_test, axis=1)
+accuracy = np.mean(predictions == y_test)
+
+print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
